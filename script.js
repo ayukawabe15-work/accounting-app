fbe42dad51c7e0c1b4a47b5accc3959489352630
ayukawabe15-work <ayukawabe15@gmail.com>
2025-09-15 03:52:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /***** Google Drive 連携 *****/
-const CLIENT_ID = "＜あなたのClientID＞";
+const CLIENT_ID = "91348359952-pns9nlvg8tr82p6ht791c31gg5meh98q.apps.googleusercontent.com";
 const SCOPES = "https://www.googleapis.com/auth/drive.file";
 
 let tokenClient;
@@ -158,3 +158,4 @@ function renderTable(){
     });
   });
 }
+
